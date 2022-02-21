e6344fc06a275ca475ff7b580ad753c6ad8dcf44
Eric Barkie <ebarkie@users.noreply.github.com>
2022-02-20 19:48:23 -0500

    add missing quote

--- Formula/netaggr.rb
+++ Formula/netaggr.rb
@@ -2,7 +2,7 @@ class Netaggr < Formula
   desc "Network aggregator/summarizer"
   homepage "https://github.com/ebarkie/netaggr"
   version "1.3.3"
-  url "https://github.com/ebarkie/netaggr/releases/download/v#{version}/netaggr-darwin-amd64.zip
+  url "https://github.com/ebarkie/netaggr/releases/download/v#{version}/netaggr-darwin-amd64.zip"
   sha256 "3c988b7896218e7aa41858b06a61af36c55f5316bb56bfcc9bbc8109a4bc4128"
 
   def install
