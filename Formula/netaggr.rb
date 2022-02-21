029de20e0a176dd54aea098a5b362455602e8be3
Eric Barkie <ebarkie@users.noreply.github.com>
2022-02-20 19:45:18 -0500

    Initial add

---
+++ Formula/netaggr.rb
@@ -0,0 +1,11 @@
+class Netaggr < Formula
+  desc "Network aggregator/summarizer"
+  homepage "https://github.com/ebarkie/netaggr"
+  version "1.3.3"
+  url "https://github.com/ebarkie/netaggr/releases/download/v#{version}/netaggr-darwin-amd64.zip
+  sha256 "3c988b7896218e7aa41858b06a61af36c55f5316bb56bfcc9bbc8109a4bc4128"
+
+  def install
+    bin.install "netaggr-darwin-amd64" => "netaggr"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# ebarkie Homebrew tap
+
+## How do I install these formulae?
+
+`brew install ebarkie/tap/<formula>`
+
+Or `brew tap ebarkie/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
