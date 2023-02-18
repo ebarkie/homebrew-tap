3884821ea408bba77eedbbc85513be617b1e98c3
Eric Barkie <ebarkie@users.noreply.github.com>
2023-02-18 13:21:20 -0500

    Brew formula update for netaggr version v1.3.7

--- Formula/netaggr.rb
+++ Formula/netaggr.rb
@@ -5,20 +5,20 @@
 class Netaggr < Formula
   desc "Network aggregator/summarizer"
   homepage "https://github.com/ebarkie/netaggr"
-  version "1.3.6"
+  version "1.3.7"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ebarkie/netaggr/releases/download/v1.3.6/netaggr_1.3.6_Darwin_arm64.tar.gz"
-      sha256 "a845a486f64e52c73064860ea6b911ec94292988d1e10fbabd6acf502ba9ed6e"
+      url "https://github.com/ebarkie/netaggr/releases/download/v1.3.7/netaggr_1.3.7_Darwin_arm64.tar.gz"
+      sha256 "8f59160a5cc5e0b96d841c2561a1a7a670bcbc499e92919b787ab33d5fd5c994"
 
       def install
         bin.install "netaggr"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ebarkie/netaggr/releases/download/v1.3.6/netaggr_1.3.6_Darwin_x86_64.tar.gz"
-      sha256 "a105bac349bfbdfd79d1fb6e625fe595e9773a99860284ba25261968dda1b0fc"
+      url "https://github.com/ebarkie/netaggr/releases/download/v1.3.7/netaggr_1.3.7_Darwin_x86_64.tar.gz"
+      sha256 "e949152bef37d9e2fa5dab955ebcc12b54005b95b0390e8eb850cba83596f65e"
 
       def install
         bin.install "netaggr"
@@ -28,16 +28,16 @@ class Netaggr < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ebarkie/netaggr/releases/download/v1.3.6/netaggr_1.3.6_Linux_x86_64.tar.gz"
-      sha256 "b614d9b009892823ca121c50c73b7d23c3ab3d50b3d245a30bda96b38732fc8c"
+      url "https://github.com/ebarkie/netaggr/releases/download/v1.3.7/netaggr_1.3.7_Linux_x86_64.tar.gz"
+      sha256 "5d391d2db8262e945013e38fdb8e70f51bc92c00b9d79da5c27161e0576614dc"
 
       def install
         bin.install "netaggr"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ebarkie/netaggr/releases/download/v1.3.6/netaggr_1.3.6_Linux_arm64.tar.gz"
-      sha256 "d11e7b7d517ac37b7a95b7442a55e18de13178e76be24c70bef2e69e474fa4ac"
+      url "https://github.com/ebarkie/netaggr/releases/download/v1.3.7/netaggr_1.3.7_Linux_arm64.tar.gz"
+      sha256 "a67b717febc50d026baa8488d138e0e37a5d5534540032f767dcafd0c6dc9776"
 
       def install
         bin.install "netaggr"
